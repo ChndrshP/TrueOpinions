418615fd81cec1f8520cbc87c0a87b34f728f29d
ChndrshP <chndrsh25@gmail.com>
2024-09-01 17:50:22 +0530

    'Resend' email verification done

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,3 +1,23 @@
 import { resend } from "@/lib/resend";
 import VerificationEmail from "../../emails/VerificationEmail";
+import { ApiResponse } from "@/types/ApiResponse";
 
+
+export async function sendVerificationEmail(
+    email: string,
+    username: string, 
+    verfiyCode: string
+) : Promise<ApiResponse>{
+    try{
+        await resend.emails.send({
+            from: 'onboarding@resend.dev',
+            to: email, 
+            subject: 'True Opinions | Verification Code',
+            react: VerificationEmail({ username, otp: verfiyCode }),
+        });
+        return {success: true, message: "Verification email send successfully"}
+    }catch(emailError){
+        console.error("Error sending veriffcation email", emailError);
+        return {success: false, message: "failed to send verification email"}
+    }
+}

b9d688c7e08d4107869850f51ae659b3ff8119f7
ChndrshP <chndrsh25@gmail.com>
2024-09-02 19:27:41 +0530

    implemented nextAuth middleware

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -4,7 +4,6 @@ import bcryprt from "bcryptjs";
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 
-
 export const authOptions: NextAuthOptions ={
     providers: [
         CredentialsProvider({
@@ -41,4 +40,31 @@ export const authOptions: NextAuthOptions ={
             }
         })
     ],
+    callbacks:{
+        async jwt({ token, user }) {
+            if(user){
+                token._id = user._id?.toString()
+                token.isVerified = user.isVerified;
+                token.isAcceptingMessages = user.isAcceptingMessages;
+                token.username = user.username; 
+            }
+            return token
+        },
+        async session({ session, token }) {
+            if(token){
+                session.user._id = token._id
+                session.user.isVerified = token.isVerified
+                session.user.isAcceptingMessages = token.isAcceptingMessages
+                session.user.username = token.username
+            }
+            return session
+          }
+    },
+    pages:{
+        signIn: '/sign-in'
+    },
+    session:{
+        strategy:"jwt"
+    },
+    secret: process.env.NEXTAUTH_SECRET
 }
